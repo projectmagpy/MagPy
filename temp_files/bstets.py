24f828b6a7e02d3b0f9a4817c136d7bcb57c0561
sarathm09 <sarathm09@gmail.com>
2015-01-24 06:16:16 +0530

    rearranged files

--- temp_files/bstets.py
+++ temp_files/bstets.py
@@ -3,4 +3,15 @@ import urllib
 
 b = bs(urllib.urlopen("http://www.crummy.com/software/BeautifulSoup/bs3/documentation.html").read())
 
-print [tag.name for tag in b.find(text="Modifying the Parse Tree").findParents()[0:3]]
+t = b.findParent(text="Modifying the Parse Tree")
+print b.findPrevious(text="Modifying the Parse Tree")
+print t
+
+x = [tag for tag in b.find(text="Modifying the Parse Tree").findParents()]
+
+id = x[0].id
+i = 0
+while id is None and i < len(x):
+	id = x[i].id
+	print x[i].name, x[i].id, x[i].class_
+	i += 1
