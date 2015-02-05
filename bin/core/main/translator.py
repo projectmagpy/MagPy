bdf349fc602ae6740507cd63ff07b115f8b70919
sarathm09 <sarathm09@gmail.com>
2015-02-05 21:11:20 +0530

    .

--- bin/core/main/translator.py
+++ bin/core/main/translator.py
@@ -1,17 +1,29 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-def translate(text, langid = 0):
-	langs = ["hi", "ml", "ru"]
-	lang = langs[langid]
-	base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
-	r = requests.get(base_url + text.replace(" ","+"))
-	html = r.content
-	result = html.split(",")[0][4:-1]
-	return result
+
+def translate(text, langid=0):
+    langs = ["hi", "ml",  "ru"]
+    if langid != -1:
+        lang = langs[langid]
+        base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
+        r = requests.get(base_url + text.replace(" ", "+"))
+        html = r.content
+        return html
+        result = html.split(",")[0][4:-1]
+        return result
+    else:
+        result = ""
+        for lang in langs:
+            base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
+            r = requests.get(base_url + text.replace(" ", "+"))
+            html = r.content
+            result += html.split(",")[0][4:-1] + "\n"
+        return result
+
 
 
 if __name__ == '__main__':
-    print translate(text=raw_input("enter text "))
+    print translate(text=raw_input("enter text "), langid=-1)
 
 
--- bin/core/nav/recursivenav.py
+++ bin/core/nav/recursivenav.py
@@ -3,45 +3,45 @@ from BeautifulSoup import BeautifulSoup
 
 
 class recursivenav():
-	def __init__(self, start, limit=0, possible_servers=[]):
-		self.starturl = start
-		self.links = [self.starturl]
-		self.visited = []
-		self.limit = limit
-		self.servers = possible_servers
-		self.data = []
+    def __init__(self, start, limit=0, possible_servers=[]):
+        self.starturl = start
+        self.links = [self.starturl]
+        self.visited = []
+        self.limit = limit
+        self.servers = possible_servers
+        self.data = []
 
-	def search(self, links=False):
-		while len(self.links) > 0 and self.limit>=len(self.visited):
-			l = self.links[0]
-			self.links.pop(0)
-			self.visited.append(l)
-			try:
-				data = urllib.urlopen(l).read()
-				self.data.append([l, data])
-				bs = BeautifulSoup(data)
-				for a in bs.findAll("a"):
-					if a['href'] != '':
-						if ".".join(self.starturl.split(".")[-2:]) in a['href']:
-							if "http://" not in a['href']:
-								link = urllib.basejoin(self.starturl, a['href'])
-								if link not in (self.links, self.visited):
-									self.links.append(link)
-							else:
-								self.links.append(a['href'])
-				if self.limit > -1:
-					self.limit -= 1
-			except:
-				pass
-		if links:
-			return self.visited
-		else:
-			return self.data
+    def search(self, links=False):
+        while len(self.links) > 0 and self.limit >= len(self.visited):
+            l = self.links[0]
+            self.links.pop(0)
+            self.visited.append(l)
+            try:
+                data = urllib.urlopen(l).read()
+                self.data.append([l, data])
+                bs = BeautifulSoup(data)
+                for a in bs.findAll("a"):
+                    if a['href'] != '':
+                        if ".".join(self.starturl.split(".")[-2:]) in a['href']:
+                            if "http://" not in a['href']:
+                                link = urllib.basejoin(self.starturl, a['href'])
+                                if link not in (self.links, self.visited):
+                                    self.links.append(link)
+                            else:
+                                self.links.append(a['href'])
+                if self.limit > -1:
+                    self.limit -= 1
+            except:
+                pass
+        if links:
+            return self.visited
+        else:
+            return self.data
 
 
 if __name__ == '__main__':
-	r = recursivenav("http://www.thehindu.com")
-	x = r.search()
-	for a in x:
-		print a
-	print len(x)
+    r = recursivenav("http://www.thehindu.com")
+    x = r.search()
+    for a in x:
+        print a
+    print len(x)
