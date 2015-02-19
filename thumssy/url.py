43bd4de08e1a86aeb7e42bdec541de9abfa557ae
sarathm09 <sarathm09@gmail.com>
2015-02-19 23:29:00 +0530

    removed unnecessary images

--- thumssy/url.py
+++
@@ -1,6 +0,0 @@
-import requests
-from BeautifulSoup import BeautifulSoup
-
-base_url = raw_input("Enter the url : ")
-content = BeautifulSoup(requests.get(base_url).text)
-print content
--- thumssy/url_list.py
+++
@@ -1,8 +0,0 @@
-import requests
-from BeautifulSoup import BeautifulSoup
-
-url_list = raw_input("Enter url list    :")
-url = url_list.split(",")
-for urls in url:
-    print urls
-    print BeautifulSoup(requests.get(urls).text)
