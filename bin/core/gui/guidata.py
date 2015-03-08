c327df8bafba009247a1988d0115b870996fabf1
sarathm09 <sarathm09@gmail.com>
2015-03-08 22:49:26 +0530

    gui updations

--- bin/core/gui/guidata.py
+++ bin/core/gui/guidata.py
@@ -1,48 +1,49 @@
 tabHeadings = ["File Management", "Page Navigation", "Selection", "Constraints", "Export"]
 btns = [
     ["Text", "Images", "Videos", "Files", "HTML"],
-    ["Search Engine", "Single URL", "URL list", "Iterative", "Recursive", "Keyword"],
+    ["Search Engine", "Single URL", "URL list", "Iterative", "Recursive"],
     ["HTML attributes", "Whole HTML", "Whole Text", "From Text", "Regular Expressions"],
     ["Size", "Count", "Consists of", "Not Consists of"],
     ["Doc", "PDF", "Text file", "Image", "Videos", "XML", "JSON"]
 ]
 
+
 inputs = [
     [
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"]
+        ["Enter Keyword or URL containing the file *"],
+        ["Enter Keyword or URL containing the file *"],
+        ["Enter Keyword or URL containing the file *"],
+        ["Enter Keyword or URL containing the file *"],
+        ["Enter Keyword or URL containing the file *"]
     ],
     [
-        ["Keyword", "Number of Results", "Extract text or just links"],
-        ["URL"],
-        ["URL " + str(i) for i in range(1, 10)],
-        ["Base URL", "Type(text/num)", "Starting character", "Ending Character"],
-        ["Base URL", "Depth", "Excluded domains", "Included domains"],
-        ["Keyword"]
+        ["Keyword *", "Number of Results", "Extract just links (y/n)"],
+        ["URL *"],
+        ["URL " + str(i) for i in range(1, 6)],
+        ["Base URL *", "Type(text/num) *", "Starting character *", "Ending Character *"],
+        ["Base URL *", "Depth", "Excluded domains", "Included domains"],
     ],
     [
         ["Tag", "Class", "Name", "ID"],
         [],
         [],
-        ["URL 1", "Text 1", "URL 2", "Text 2", "URL 3", "Text 3"],
-        ["Pattern"]
+        ["URL 1 *", "Text 1 *", "URL 2 *", "Text 2 *", "URL 3", "Text 3"],
+        ["Pattern *"]
     ],
     [
         ["Size", "Type"],
-        ["Regular Expression"],
-        ["Words"],
-        ["Words"]
+        ["Pattern *"],
+        ["Words *"],
+        ["Words *"]
     ],
     [
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"],
-        ["If not Based on Previous Keyword, enter Keyword", "URL"]
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"],
+        ["If not Based on Search results, enter Keyword/URL"]
+
     ]
 ]
