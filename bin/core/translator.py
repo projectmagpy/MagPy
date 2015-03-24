6499d219c8b07dd8c72c13fdeb9a361f3cc890d8
T90 <sarathm09@gmail.com>
2015-03-24 20:03:16 +0530

    .

--- bin/core/translator.py
+++ bin/core/translator.py
@@ -4,24 +4,28 @@ from BeautifulSoup import BeautifulSoup
 
 def translate(text, langid=0):
     langs = ["hi", "ml",  "ru"]
-    if langid != -1:
-        lang = langs[langid]
-        base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
-        r = requests.get(base_url + text.replace(" ", "+"))
-        html = r.content
-        return html
-        result = html.split(",")[0][4:-1]
-        return result
-    else:
-        result = ""
-        for lang in langs:
-            base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
-            r = requests.get(base_url + text.replace(" ", "+"))
-            html = r.content
-            result += html.split(",")[0][4:-1] + "\n"
-        return result
+    # if langid != -1:
+    lang = langs[langid]
+    base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
+    url = base_url + text.replace(" ", "+")
+    print url
+    r = requests.get(url)
+    html = r.content
+    result = html.split(",")[0][4:-1]
+    return result
+    # else:
+    #     result = ""
+    #     for lang in langs:
+    #         base_url = "https://translate.google.co.in/translate_a/single?client=t&sl=auto&tl=" + lang + "&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&rom=0&ssel=0&tsel=4&tk=518745|178722&q="
+    #         r = requests.get(base_url + text.replace(" ", "+"))
+    #         html = r.content
+    #         result += html.split(",")[0][4:-1] + "\n"
+    #     return result
 
 if __name__ == '__main__':
-    print translate(text=raw_input("enter text "), langid=-1)
+    li = open("dict.txt").read().split("\n")
+    for text in li:
+        text = ''.join([i if ord(i) < 128 else ' ' for i in text]).replace(" ", "").lower()
+        print translate(text=text, langid=1)
 
 
