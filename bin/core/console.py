7b75bb9a8f08b355d9fdd78b7a8b3038c9e68210
T90 <sarathm09@gmail.com>
2015-05-17 23:09:26 +0530

    removed files

--- bin/core/console.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'console_log.ui'
-#
-# Created: Mon Mar 16 00:21:37 2015
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
-
