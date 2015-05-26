e88e73e36877a81dc3e40f5b9cf20b46f090443b
T90 <sarathm09@gmail.com>
2015-05-26 21:25:00 +0530

    .

--- temp_files/extract_images.py
+++ temp_files/extract_images.py
@@ -9,53 +9,56 @@ import hashlib
 
 
 def searchPic(term):
-	img_list = getPic(term)
-	if len(img_list) > 0:
-		for img in img_list:
-			savePic(img)
-	print "done..."
+    img_list = getPic(term)
+    if len(img_list) > 0:
+        for img in img_list:
+            savePic(img)
+    print "done..."
 
 
 def getPic(search):
-	search = search.replace(" ", "%20")
-	try:
-		browser = mechanize.Browser()
-		browser.set_handle_robots(False)
-		browser.addheaders = [('User-agent', 'Mozilla')]
+    search = search.replace(" ", "%20")
+    # try:
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.addheaders = [('User-agent', 'Mozilla')]
 
-		htmltext = browser.open(
-			"https://www.google.com/search?site=imghp&tbm=isch&source=hp&biw=1414&bih=709&q=" + search + "&oq=" + search)
-		img_urls = []
-		formatted_images = []
-		soup = BeautifulSoup(htmltext)
-		results = soup.findAll("a")
-		for r in results:
-			try:
-				if "imgres?imgurl" in r['href']:
-					img_urls.append(r['href'])
-			except:
-				a = 0
-		for im in img_urls:
-			refer_url = urlparse(str(im))
-			image_f = refer_url.query.split("&")[0].replace("imgurl=", "")
-			formatted_images.append(image_f)
+    htmltext = browser.open(
+        "https://www.google.com/search?site=&tbm=isch&source=hp&biw=995&bih=775&gs_l=img.3..0l10.15017.15339.0.25312.4.4.0.0.0.0.146.290.0j2.2.0.msedr...0...1ac.1.64.img..2.2.290.3rpJmTHh5dU&q=" + search + "&oq=" + search)
+    img_urls = []
+    formatted_images = []
+    soup = BeautifulSoup(htmltext)
+    results = soup.findAll("a")
+    for r in results:
+        try:
+            if "imgres?imgurl" in r['href']:
+                print r
+                img_urls.append(r['href'])
+        except:
+            a = 0
+    for im in img_urls:
+        print img_urls
+        refer_url = urlparse(str(im))
+        image_f = refer_url.query.split("&")[0].replace("imgurl=", "")
+        formatted_images.append(image_f)
 
-		return formatted_images
+    return formatted_images
 
-	except:
-		return []
+    # except:
+    #     return []
 
 
 def savePic(url):
-	hs = hashlib.sha224(url).hexdigest()
-	file_extension = url.split(".")[-1]
-	uri = ""
-	dest = uri + hs + "." + file_extension
-	print dest
-	try:
-		urllib.urlretrieve(url, dest)
-	except:
-		print "save failed"
+    print url
+    hs = hashlib.sha224(url).hexdigest()
+    file_extension = url.split(".")[-1]
+    uri = ""
+    dest = uri + hs + "." + file_extension
+    print dest
+    try:
+        urllib.urlretrieve(url, dest)
+    except:
+        print "save failed"
 
 
 searchPic(raw_input("Enter keyword : "))
